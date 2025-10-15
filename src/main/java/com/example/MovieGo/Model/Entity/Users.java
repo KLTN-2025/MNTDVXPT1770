18aad76d09bde08e071f27a22d62e743a46211a8
Trung-Be-Ac <ahoangtrung12345@gmail.com>
2025-10-15 17:29:05 +0700

    UserEntity

---
+++ src/main/java/com/example/MovieGo/Model/Entity/Users.java
@@ -0,0 +1,19 @@
+package com.example.MovieGo.Model.Entity;
+
+import java.time.LocalDateTime;
+
+import lombok.*;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Users {
+    private int userId;
+    private String fullName;
+    private String email;
+    private String phone;
+    private String role;
+    private String status;  
+    private LocalDateTime createdAt;
+}
