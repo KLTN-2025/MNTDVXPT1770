fbadea1275163db1b5bed21c42cc5bd58d8cca27
Trung-Be-Ac <ahoangtrung12345@gmail.com>
2025-11-10 18:08:09 +0700

    Khởi tạo lại repo trống

---
+++ src/test/java/com/example/MovieGo/MovieGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MovieGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
